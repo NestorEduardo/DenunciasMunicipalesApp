a7d2862b9024f722b0fb5ec26470c9257b4aa5e6
NestorEduardo <nestoredelacruz@gmail.com>
2017-04-22 19:16:41 -0400

    Consumied the ApiService

--- DenunciasMunicipalesApp/DenunciasMunicipalesApp/ViewModels/MainViewModel.cs
+++ DenunciasMunicipalesApp/DenunciasMunicipalesApp/ViewModels/MainViewModel.cs
@@ -1,6 +1,30 @@
-﻿namespace DenunciasMunicipalesApp.ViewModels
+﻿using DenunciasMunicipalesApp.Services;
+
+namespace DenunciasMunicipalesApp.ViewModels
 {
     public class MainViewModel
     {
+        #region Attributes
+
+        private ApiService apiService;
+
+        #endregion
+
+        #region Properties
+
+        #endregion
+
+        #region Constructors
+
+        public MainViewModel()
+        {
+            apiService = new ApiService();
+        }
+
+        #endregion
+
+        #region Methods
+
+        #endregion
     }
 }
