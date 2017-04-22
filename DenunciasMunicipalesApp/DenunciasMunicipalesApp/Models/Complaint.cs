3ffcfaaf83f4e42570796507cf62be62388d36e4
NestorEduardo <nestoredelacruz@gmail.com>
2017-04-22 19:13:13 -0400

    Added the Complaint model

---
+++ DenunciasMunicipalesApp/DenunciasMunicipalesApp/Models/Complaint.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace DenunciasMunicipalesApp.Models
+{
+    class Complaint
+    {
+        public int ComplaintId { get; set; }
+
+        public string Description { get; set; }
+
+        public string CaseAddress { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string CreatedBy { get; set; }
+    }
+}
