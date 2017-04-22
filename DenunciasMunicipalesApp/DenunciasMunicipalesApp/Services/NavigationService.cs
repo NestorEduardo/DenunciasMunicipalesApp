66c78e0d9e86ef1f63767d98290299ab302d5454
NestorEduardo <nestoredelacruz@gmail.com>
2017-04-22 19:39:52 -0400

    Added the NavigationService

---
+++ DenunciasMunicipalesApp/DenunciasMunicipalesApp/Services/NavigationService.cs
@@ -0,0 +1,25 @@
+﻿using DenunciasMunicipalesApp.Pages;
+using System.Threading.Tasks;
+
+namespace DenunciasMunicipalesApp.Services
+{
+    public class NavigationService
+    {
+        public async Task Navigate(string pageName)
+        {
+            switch (pageName)
+            {
+                case "NewComplaintPage":
+                    await App.Current.MainPage.Navigation.PushAsync(new NewComplaintPage());
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public async Task Back()
+        {
+            await App.Current.MainPage.Navigation.PopAsync();
+        }
+    }
+}
